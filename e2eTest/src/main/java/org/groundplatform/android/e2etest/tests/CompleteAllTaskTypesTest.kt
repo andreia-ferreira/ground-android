0a946b2b2480fc8a5dd24112ac835c8aca9325d8
andreia <andreia.mp.ferreira@gmail.com>
2026-01-20 16:31:17 +0100

    finish test on home screen

--- e2eTest/src/main/java/org/groundplatform/android/e2etest/tests/CompleteAllTaskTypesTest.kt
+++ e2eTest/src/main/java/org/groundplatform/android/e2etest/tests/CompleteAllTaskTypesTest.kt
@@ -19,6 +19,7 @@ import org.groundplatform.android.e2etest.robots.SignInRobot
 import org.groundplatform.android.e2etest.robots.SurveySelectorRobot
 import org.groundplatform.android.e2etest.robots.TermsOfServiceRobot
 import org.groundplatform.android.ui.main.MainActivity
+import org.groundplatform.android.ui.map.gms.features.TEST_MARKER_TAG
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -89,6 +90,9 @@ class CompleteAllTaskTypesTest {
     with(DataCollectionRobot(testDriver)) {
       dismissInstructions()
       runTasks(TestConfig.TEST_LIST_ALL_TASK_TYPES_EXCEPT_DRAW_AREA)
+    }
+    with(HomeScreenRobot(testDriver)) {
+      moveMap()
     }
   }
 
